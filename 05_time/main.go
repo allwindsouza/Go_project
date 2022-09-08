67b5f560c74cdad680a1f5f0d1fc6fdfbf356be2
Allwin Dsouza <aldsouza4@gmail.com>
2022-09-08 14:47:14 +0530

    time

--- 05_time/main.go
+++ 05_time/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	fmt.Printf("Welcome to time study of golang \n")
+
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+
+	fmt.Println(presentTime.Format("01-02-2006"))
+
+	createDate := time.Date(2020, time.April, 10, 23, 23, 0, 0, time.UTC)
+
+	fmt.Println(createDate)
+	fmt.Println(createDate.Format("01-02-2006 Monday"))
 
 }
